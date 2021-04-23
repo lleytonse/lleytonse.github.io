098e43a0388095e55f372c1227f30cf33a33ea51
lleytonse <70021199+lleytonse@users.noreply.github.com>
2021-04-23 18:20:28 -0400

    cleaned existing files / prepared file & directory structure

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
+<!-- 
+
+    Name:   Lleyton Seymour
+    Date:   Apr. 2021
+    File:   README.md
+    Notes:  'Still under construction' 
+
+-->
+
+
 # Personal Website ![oops](https://drive.google.com/uc?export=view&id=1wiBaJPU5q6XbMJDovQJ-SY6m006-GdpY)
 
 ### See: [LleytonSeymour&trade;.com](https://lleytonseymour.tumblr.com/)
 
 My personal website (soon-to-be) hosted on [github pages](https://pages.github.com/).
 
-
+## Style Guide
+### Colors
+### Font
 
---
+++ timeUpdate.js
@@ -0,0 +1,32 @@
+/* Time Update Function */
+            
+function addZero(i) { //for single digit minutes
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+
+function updateClock() {
+    var date = new Date();
+    var hours = date.getHours();
+    var hours = (hours+24)%24; 
+    var mins = addZero(date.getMinutes());
+    var mid='AM';
+    
+    if(hours==0) { //exception case for 12:00 AM
+        hours=12;
+    }
+    else if(hours>12){
+        hours=hours%12;
+        mid='PM';
+    }
+   
+        //formatting string for display
+    var time =  ''.concat('<b> Today </b>', hours, ":", mins, ' ', mid);
+    document.getElementById('Time').innerHTML = time;
+
+        //call function every 10 seconds
+    setTimeout(updateClock, 10000);
+}
+updateClock();
