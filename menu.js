0c23bdfc62dff5bad3a953db994659912a3dab06
lleytonse <70021199+lleytonse@users.noreply.github.com>
2021-05-23 16:55:05 -0400

    temp fix for returning home

--- menu.js
+++ menu.js
@@ -8,14 +8,17 @@ function sleep(ms) {
 
 for(var i = 0; i < li_elements.length; i++) {
     li_elements[i].addEventListener("click", function(){
+
         li_elements.forEach(function(li){
             li.classList.remove("active");
         })
+
         this.classList.add("active");
         var li_value = this.getAttribute("data-li");
         item_elements.forEach(function(item){
             item.style.display = "none";
         })
+
         if(li_value == "about") {
             document.querySelector(".Page-Container").style.transform = "translate(-25vw)";
             document.querySelector(".Page-Container").style.transition = "transform 1.5s ease";
@@ -39,7 +42,12 @@ for(var i = 0; i < li_elements.length; i++) {
         }
 
         else if(li_value == "home") {
-            document.querySelector("." + li_value).style.display = "none";
+            //document.querySelector("." + li_value).style.display = "none";
+
+            document.querySelector(".Page-Container").style.transform = "translate(0vw)";
+            document.querySelector(".Page-Container").style.transition = "transform 1.5s ease";
+
+            //setTimeout(() => { document.querySelector("." + li_value).style.display = "flex"; }, 500)
         }
 
         else {
@@ -49,3 +57,4 @@ for(var i = 0; i < li_elements.length; i++) {
         
     })
 }
+
